69e8f3d946680697b674e2bcee18c35a9a91898b
shivanshvermaa <shivansh.verma8080@gmail.com>
2020-06-07 20:49:29 +0530

    main page connected with database login and registration pages created without proper functionality

--- app/src/main/java/com/example/expense/Registeration.java
+++ app/src/main/java/com/example/expense/Registeration.java
@@ -3,7 +3,12 @@ package com.example.expense;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registeration extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class Registeration extends AppCompatActivity {
     EditText email;
     EditText password;
     EditText passwordconfirm;
+    Button register;
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +33,24 @@ public class Registeration extends AppCompatActivity {
         email = findViewById(R.id.reg_email_edittext);
         password = findViewById(R.id.reg_pass_edittext);
         passwordconfirm = findViewById(R.id.reg_passconf_edittext);
+        register = findViewById(R.id.reg_register_button);
 
-        //Some data
+        fAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String first = fn.getText().toString().trim();
+                String last = ln.getText().toString().trim();
+                String mail = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                String pass_c = passwordconfirm.getText().toString().trim();
+            }
+        });
+
+
+
+
 
     }
 }
